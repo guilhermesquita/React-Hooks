6fbc8fc91d59ba2ab44916eba22df8ab3fcc7b08
Guilherme Mesquita <guilhermer_mesquita@hotmail.com>
2022-05-18 11:29:48 -0400

    useState

---
+++ React-Hooks/src/App.tsx
@@ -0,0 +1,27 @@
+//Neste exemplo faremos um CONTADOR. Que irá diminiur / subtrair
+
+import { useState } from 'react'
+
+function App() {
+ const [counter, setCounter] = useState(0); //Ou seja, o valor de counter = 0. o SetCounter serve apenas para alterar o valor de counter futuramente.
+
+ function handlePlus(){
+   setCounter ((prevState) => prevState +1); //
+ }
+
+ function handleMinus(){
+  setCounter (counter - 1);
+}
+
+  return (
+
+ <>
+  <h1>{counter}</h1>
+  <button onClick={handlePlus}>+</button>
+  <button onClick= {handleMinus}>-</button>
+ </>
+
+ )
+}
+
+export default App
